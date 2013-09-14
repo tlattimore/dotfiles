aae55b528de8a4b3c6623f6a5ba675947d550013
tlattimore <lattimorethomas@gmail.com>
2013-09-14 19:06:25 -0400

    Removing uneaded scripts and updating prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,4 +2,4 @@ autoload colors && colors
 
 PROMPT='%{%f%k%b%}
 %{%K{${bkg}}%B%F{green}%}%n%{%B%F{blue}%}@%{%B%F{cyan}%}%m%{%B%F{green}%} %{%b%F{yellow}%K{${bkg}}%}%~%{%B%F{green}%}$(git_prompt_info)%E%{%f%k%b%}
-%{%K{${bkg}}%}$(_prompt_char)%{%K{${bkg}}%} %#%{%f%k%b%} '
+%{%K{${bkg}}%}%{%K{${bkg}}%} >%{%f%k%b%} '
