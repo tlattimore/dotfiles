d2c5f258fc56cb89731ae4f6e20c273822e915dd
tlattimore <lattimorethomas@gmail.com>
2013-09-17 12:03:13 -0400

    Updating zsh prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,5 +1,5 @@
 autoload colors && colors
 
 PROMPT='%{%f%k%b%}
-%{%K{${bkg}}%B%F{green}%}%n%{%B%F{blue}%}@%{%B%F{cyan}%}%m%{%B%F{green}%} %{%b%F{yellow}%K{${bkg}}%}%~%{%B%F{green}%}$(git_prompt_info)%E%{%f%k%b%}
+(%{%K{${bkg}}%B%F{green}%}%n%{%B%F{blue}%}) %{%B%F{green}%}%{%b%F{yellow}%K{${bkg}}%}%~%{%B%F{green}%}$(git_prompt_info)%E%{%f%k%b%}
 %{%K{${bkg}}%}%{%K{${bkg}}%} >%{%f%k%b%} '
