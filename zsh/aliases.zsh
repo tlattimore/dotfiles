b4486dfe286366e1e2526ba8366addd99bfe6ef2
Thomas Lattimore <thomas@lattimore.me>
2020-02-14 16:12:28 -0500

    Migrate to new autocomplete and monokai theme
    New stuff.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,8 @@ alias se='grep -rin'
 alias reload!='. ~/.zshrc'
 alias cppath='pwd | pbcopy'
 alias mux="tmuxinator"
+alias rand="env LC_CTYPE=C tr -dc \"A-Za-z0-9_\!\@#\$%^&*()-+=\" < /dev/urandom | head -c 32 | xargs"
+alias p="platform"
 
 # Create a new directory and enter it
 function md() {
