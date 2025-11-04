0bc8f24c9001e3f41c3adb9580a4a8a4fe4068c2
Thomas Lattimore <thomas@lattimore.me>
2025-11-04 12:40:04 -0500

    bat alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,7 @@ alias reload!='. ~/.zshrc'
 alias cppath='pwd | pbcopy'
 alias mux="smug"
 alias rand="env LC_CTYPE=C tr -dc \"A-Za-z0-9_\!\@#\$%^&*()-+=\" < /dev/urandom | head -c 32 | xargs"
+alias cat="bat"
 
 # Create a new directory and enter it
 function md() {
